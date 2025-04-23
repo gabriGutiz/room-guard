327e8e0166f017f4ff3f9ea679f850462ebe1aef
gabriGutiz <gabri.gutiz@gmail.com>
2025-04-23 13:03:02 -0300

    fix: install docker compose in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,14 @@ sudo apt-get update && \
 	sudo apt-get install nginx -y && \
 	sudo apt-get install docker.io -y
 
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "CLONING GIT"
 git clone https://$1:$2@github.com//gabriGutiz/room-guard.git
 
 echo "BUILDING DOCKER"
-sudo docker compose up -d
+sudo docker-compose up -d
 
 #echo "NGINX"
 #sudo systemctl restart nginx
