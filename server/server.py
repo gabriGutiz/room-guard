d4008d9fea85bb990bb139f32a7f939736f68b57
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2025-06-09 17:43:45 -0300

    feat: implement put to dynamo

--- server/server.py
+++ server/server.py
@@ -1,14 +1,16 @@
-import base64
-from io import BytesIO
+import boto3
 import json
+import logging
 from mqtt import configure_sub as mqtt_sub
 import os
-from PIL import Image
 import sys
-from ultralytics import YOLO
+
+from img import is_room_empty
 
 
-BROKER_HOST = os.environ.get("BROKER_HOST", "ec2-3-82-138-13.compute-1.amazonaws.com")
+LOGS_FILE_NAME="server.log"
+
+BROKER_HOST = os.environ.get("BROKER_HOST", "ec2-54-163-19-208.compute-1.amazonaws.com")
 BROKER_PORT = os.environ.get("BROKER_PORT", 1883)
 BROKER_USER = "user1"
 BROKER_PWD = "user1"
@@ -16,26 +18,63 @@ BROKER_PWD = "user1"
 ROOM_TOPIC = os.environ.get("ROOM_TOPIC", "paho/room")
 MSG_QOS = 0
 
-YOLO_MODEL = "yolov8n.pt"
+AWS_REGION = os.environ.get("aws_region", "us-east-1")
 
 
-model = YOLO(YOLO_MODEL)
+dynamodb = boto3.resource("dynamodb", region_name=AWS_REGION)
+
+room_guard_table = dynamodb.Table("room-guard")
 
 
-def process_message(msg):
-    content = json.load(msg)
-    img = Image.open(BytesIO(base64.b64decode(content.get("img"))))
+class RoomState:
+    room_id: str
+    time: str
+    room_is_empty: bool
+    door_is_closed: bool
+
+
+def _extract_msg_state(msg_body: str) -> RoomState:
+    body = json.loads(msg_body)
+
+    state = RoomState()
+    state.room_id = body.get("room_id")
+    state.time = body.get("time")
+    state.room_is_empty = is_room_empty(body.get("msg"))
+    state.door_is_closed = body.get("door_is_closed")
+
+    return state
+
+
+def process_message(msg_body: str) -> None:
+    state = _extract_msg_state(msg_body)
+
+    if state.room_is_empty:
+        logging.info("THERE IS PEOPLE IN THE ROOM")
+
+    room_guard_table.put_item(
+        Item={
+            "id": f"room-{state.room_id}",
+            "type": "room",
+            "room_is_empty": state.room_is_empty,
+            "datetime": state.time
+        }
+    )
 
 
 def mqtt_on_message(client, userdata, msg):
     global global_message
     if msg.topic == ROOM_TOPIC:
-        print(msg.topic + " " + str(msg.payload))
-        process_message(msg.payload)
+        try:
+            #print(msg.topic + " " + str(msg.payload))
+            process_message(msg.payload)
+        except Exception as e:
+            logging.exception("Error processing message")
 
 
 if __name__ == "__main__":
     try:
+        logging.basicConfig(filename=LOGS_FILE_NAME, level=logging.INFO)
+
         mqtt_sub(
             BROKER_HOST,
             BROKER_PORT,
--- utils/connect-ec2.sh
+++ utils/connect-ec2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo ssh -i ../.ec2/room-guard.pem ubuntu@ec2-3-82-138-13.compute-1.amazonaws.com
+sudo ssh -i ../.ec2/room-guard-key.pem ubuntu@ec2-54-158-248-228.compute-1.amazonaws.com
 
